a00d2886945f2f77866108bbf38983dd72052436
thazsobral <thazsobral@gmail.com>
2024-05-22 23:47:54 -0300

    add new classes

---
+++ 2-programacao-extrema-java/string/UseStringBuffer.java
@@ -0,0 +1,21 @@
+package string;
+
+public class UseStringBuffer {
+    public static void main(String[] args) {
+        String name = "Thalles";
+        String lastname = "Sobral";
+        String name1 = name + lastname;
+        String name2;
+        
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(name);
+        buffer.append(lastname);
+
+        System.out.println(name1);
+        System.out.println(buffer); // this clear next time
+        name2 = buffer.toString();
+
+        System.out.println(name1 == name2);
+        System.out.println(name1.equals(name2));
+    }
+}
