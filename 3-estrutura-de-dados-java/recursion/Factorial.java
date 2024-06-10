d7ec8236e9627be16398978132aebb2ec055ed3f
thazsobral <thazsobral@gmail.com>
2024-06-10 19:37:17 -0300

    add new class recurs

--- 3-estrutura-de-dados-java/recursion/Factorial.java
+++ 3-estrutura-de-dados-java/recursion/Factorial.java
@@ -1,17 +1,26 @@
 package recursion;
 
 public class Factorial {
-    public static int factorial(int value) {
+    public static int factorialWithTail(int value) { //exemplo de recursão com cauda
         if(value == 0) { // regra 1: quando parar a recursão? (value == 0)
             return 1;
         } else {
-            return value * factorial(value-1); //regra 2: o que fazer com a recursao? (value *) e 3: como minuir a jornada recursiva? (factorial(value-1))
+            return value * factorialWithTail(value-1); //regra 2: o que fazer com a recursao? (value *) e 3: como minuir a jornada recursiva? (factorial(value-1))
+        }
+    }
+
+    public static void taillessFactorial(int value, int factor) { //exemplo de recursão sem cauda
+        if(value > 1) { // 
+            taillessFactorial(value - 1, factor * value); //
+        } else {
+            System.out.println("O fatorial é "+factor);
         }
     }
 
     public static void main(String[] args) {
         int result, value = 5;
-        result = factorial(value);
-        System.out.println(result);
+        //result = factorialWithTail(value);
+        //System.out.println(result);
+        taillessFactorial(value, 1);
     }
 }
